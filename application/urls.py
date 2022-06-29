9f9bfd39faaf2d3eca7e6764dd7112c915224819
majidakbaria <majidakbari.al@gmail.com>
2022-06-29 08:24:35 -0700

    a

--- application/urls.py
+++ application/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from application.views import *
+
+app_name = "application"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view),
-    path("contact", contact_view),
-    path("about", about_view)
+    path('', index_view , name='index'),
+    path("contact", contact_view , name='contact'),
+    path("about", about_view , name='about')
 ]
